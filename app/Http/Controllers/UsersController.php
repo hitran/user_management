a98016cbd854e489748134b01ec13e5d12c9a5e9
Hi Tran <thuchitran@gmail.com>
2020-05-09 15:26:13 +0700

    trying to connect to db

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,13 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
 
 class UsersController extends Controller
 {
     public function index()
     {
-        $userJson = file_get_contents(base_path("resources/users/users.json"));
-        $users = json_decode($userJson, true);
-        return view('users.index')->with('users', $users);
+        // $userJson = file_get_contents(base_path("resources/users/users.json"));
+        // $users = json_decode($userJson, true);
+        $users = DB::table('users')->get();
+        dd($users);
+        //return view('users.index')->with('users', $users);
     }
 }
